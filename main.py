f0b24dba992b9a061ff5fb3aab9ddae9e802a148
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-08-24 19:07:50 +0900

    Update 2 Files
    텔레그램

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+import Adafruit_DHT
+import os
 
-#
+sensor = Adafruit_DHT.DHT11
+pin = 2
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+token = os.environ['TOKEN']
+
+# updater 
+updater = Updater(token=token, use_context=True)
+dispatcher = updater.dispatcher
+
+
+# command hander
+def 온도(update, context):    
+    if humidity is not None and temperature is not None:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="문제가 발생했습니다. 잠시후 다시 시도해주세요!")
+
+start_handler = CommandHandler('온도', 온도)
+dispatcher.add_handler(start_handler)
+
+# polling
+updater.start_polling()
