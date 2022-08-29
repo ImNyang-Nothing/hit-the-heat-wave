a8469b844bdfb258685896a606904031c4c462b6
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-08-29 16:53:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
-import discord
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
 import Adafruit_DHT
 import os
-import time
-import RPi.GPIO as GPIO
 
 sensor = Adafruit_DHT.DHT11
 pin = 2
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-user_id = []
+token = os.environ['TOKEN']
+telegram_id = ["5745787280"]
 
-bot = discord.Bot()
+# updater 
+updater = Updater(token=token, use_context=True)
+dispatcher = updater.dispatcher
 
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-    for i in range(len(user_id)):
-        user = await bot.fetch_user(user_id[i])
-        await user.send("✅ㅣ봇이 준비되었습니다!")
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="슬커로 알아보세요!"))
-
-
-@bot.slash_command()
-async def 온습도_측정(ctx):
+print("Started Bot~")
+# command hander
+def temphumidity(update, context):
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
-        await ctx.respond('온도={0:0.1f}*C  습도={1:0.1f}%'.format(temperature, humidity))
+        context.bot.send_message(chat_id=update.effective_chat.id, text='온도={0:0.1f}*C  습도={1:0.1f}%'.format(temperature, humidity))
     else:
-        await ctx.respond("문제가 발생했습니다. 잠시후 다시 시도해주세요!")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="문제가 발생했습니다. 잠시후 다시 시도해주세요!")
 
-bot.run(str(os.getenv('TOKEN')))
+start_handler = CommandHandler('show_status', temphumidity)
+dispatcher.add_handler(start_handler)
+
+# polling
+updater.start_polling()
