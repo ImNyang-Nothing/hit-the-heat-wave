c9e72fe736964896f8d6507994950cd1a1e780a8
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-09-12 19:31:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+import discord
+import RPi.GPIO as GPIO
+import datetime
+import time
+import tokenpython
 import Adafruit_DHT
 import os
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21,GPIO.IN)
+
+def speak(text):
+    os.system(f"./speech.sh {text}")
+
+bot = discord.Bot()
 
 sensor = Adafruit_DHT.DHT11
 pin = 2
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-token = os.environ['TOKEN']
-telegram_id = ["5745787280"]
+discord_id = [""]
 
-# updater 
-updater = Updater(token=token, use_context=True)
-dispatcher = updater.dispatcher
+class ResetButton(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+    @discord.ui.button(label="초기화", style=discord.ButtonStyle.primary, emoji="🔄") # Create a button with the label "😎 Click me!" with color Blurple
+    async def button_callback(self, button, interaction):
+        await interaction.response.send_message("초기화 신호를 보냈습니다!") # Send a message when the button is clicked
+        nonecount = 0
 
-print("Started Bot~")
+@bot.event
+async def on_ready():
+    print("Started Bot~")
 # command hander
-def temphumidity(update, context):
+@bot.command()
+async def show_status(ctx):
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='온도={0:0.1f}*C  습도={1:0.1f}%'.format(temperature, humidity))
+        await ctx.respond('온도={0:0.1f}*C  습도={1:0.1f}%'.format(temperature, humidity))
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="문제가 발생했습니다. 잠시후 다시 시도해주세요!")
+        await ctx.respond('문제가 발생했습니다. 잠시후 다시 시도해주세요!')
 
-for i in telgram_id:
-    bot.send_message(chat_id={i}, text='Hello telegram bot!')
+@bot.command()
+async def show_sos(ctx):
+    if nonecount != 10:
+        await ctx.respond(f"💚 아직까진 안전합니다! 위험지수 : {nonecount}", view=ResetButton()) # Send a message with our View class that contains the button
+    else:
+        await ctx.respond(f"❤️ 현재 감지되고 있지 않습니다!!!!! 위험지수 : {nonecount}", view=ResetButton())
 
-start_handler = CommandHandler('show_status', temphumidity)
-dispatcher.add_handler(start_handler)
+@bot.command()
+async def test(ctx):
+    speak("남현석")
+    await ctx.respond("잘 들어보세요... 무슨 소리가 나지 않나요?")
 
-# polling
-updater.start_polling()
+bot.run(tokenpython.token)
